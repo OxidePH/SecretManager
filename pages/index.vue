b71b81a1364443c8e17c98dc7848b44353278b8f
Katherine Maamo <katherinemaamo7@gmailcom>
2025-02-15 02:10:57 +0800

    # Import EProject

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,54 @@
 <template>
-	<div class="container mx-auto text-gray-300">
-		<div class="flex flex-col items-center">
-			<div class="w-full flex flex-wrap space-x-8 justify-center mt-24">
-				<a target="_blank" class="flex items-center" href="https://nuxt.com/">
-					<img class="w-44" src="https://nuxt.com/assets/design-kit/logo/icon-green.svg" alt="nuxt3" >
-				</a>
-				<a target="_blank" class="flex items-center" href="https://tauri.app/">
-					<img class="w-44" src="https://raw.githubusercontent.com/tauri-apps/tauri/HEAD/app-icon.png" alt="tauri app" >
-				</a>
-				<a target="_blank" class="flex items-center" href="https://tailwindcss.com/">
-					<img class="w-44" src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.79614a5f61617ba49a0891494521226b.svg" alt="tailwind">
-				</a>
+	<div>
+		<section
+			class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply"
+		>
+			<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+				<h1
+					class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl"
+				>
+					We invest in the world’s potential
+				</h1>
+				<p
+					class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48"
+				>
+					Here at Flowbite we focus on markets where technology, innovation, and
+					capital can unlock long-term value and drive economic growth.
+				</p>
+				<div
+					class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0"
+				>
+					<a
+						href="#"
+						class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+					>
+						Get started
+						<svg
+							class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 14 10"
+						>
+							<path
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M1 5h12m0 0L9 1m4 4L9 9"
+							/>
+						</svg>
+					</a>
+					<a
+						href="#"
+						class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
+					>
+						Learn more
+					</a>
+				</div>
 			</div>
-			<p class="text-2xl">
-				Nux3 & Tauri & TailwindCSS
-			</p>
-			<p class="text-gray-400">by <a href="https://github.com/MakotoPD" target="_blank" class="underline">MakotoPD</a></p>
-		</div>
-		
-
-  	</div>
+		</section>
+	</div>
 </template>
+
+<script setup lang="ts"></script>
---
+++ pages/secrets.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="p-4 flex justify-center items-center">
+		<SecretTable />
+	</div>
+</template>
+
+<script setup lang="ts">
+import SecretTable from "~/components/secret-table.vue";
+</script>
