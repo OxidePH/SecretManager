f16f882cdb3434a40f8c745322328460995bc86b
Katherine Maamo <katherinemaamo7@gmailcom>
2025-02-16 12:56:07 +0800

    developer button

--- components/titlebar.vue
+++ components/titlebar.vue
@@ -1,66 +1,75 @@
 <template>
-  <div data-tauri-drag-region class="titlebar">
-    <div class="titlebar-buttons">
-      <div class="titlebar-button close" @click="handleClose"></div>
-      <div class="titlebar-button minimize" @click="handleMinimize"></div>
-    </div>
-    <div class="app-name">Oxide Secret Manager</div>
-  </div>
+	<div data-tauri-drag-region class="titlebar">
+		<div class="titlebar-buttons">
+			<div class="titlebar-button close" @click="handleClose"></div>
+			<div class="titlebar-button minimize" @click="handleMinimize"></div>
+			<div class="titlebar-button developer" @click="handleDeveloper"></div>
+		</div>
+		<div class="app-name">Oxide Secret Manager</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { appWindow } from '@tauri-apps/api/window'
+import { appWindow } from "@tauri-apps/api/window";
 
 const handleMinimize = () => {
-  appWindow.minimize()
-}
+	appWindow.minimize();
+};
 
 const handleClose = () => {
-  appWindow.close()
+	appWindow.close();
+};
+
+const handleDeveloper = () => {
+  
 }
 </script>
 
 <style scoped>
 .titlebar {
-  z-index: 999999;
-  height: 30px;
-  background: #111827;
-  user-select: none;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  padding: 0 10px;
+	z-index: 999999;
+	height: 30px;
+	background: #111827;
+	user-select: none;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	padding: 0 10px;
 }
 
 .titlebar-buttons {
-  display: flex;
-  gap: 8px;
+	display: flex;
+	gap: 8px;
 }
 
 .titlebar-button {
-  width: 12px;
-  height: 12px;
-  border-radius: 50%;
-  background-color: #ff5f57;
-  cursor: pointer;
+	width: 12px;
+	height: 12px;
+	border-radius: 50%;
+	background-color: #ff5f57;
+	cursor: pointer;
 }
 
 .titlebar-button.minimize {
-  background-color: #ffbd2e;
+	background-color: #ffbd2e;
+}
+
+.titlebar-button.developer {
+	background-color: #51ff2e;
 }
 
 .titlebar-button:hover {
-  opacity: 0.8;
+	opacity: 0.8;
 }
 
 .app-name {
-  margin-left: auto;
-  padding-right: 3px;
-  font-size: 14px;
-  color: #f3f3f4;
+	margin-left: auto;
+	padding-right: 3px;
+	font-size: 14px;
+	color: #f3f3f4;
 }
 </style>
---
+++ pages/news.vue
@@ -0,0 +1,11 @@
+<template>
+	<div></div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+	middleware: "setup-middleware",
+});
+</script>
+
+<style></style>
